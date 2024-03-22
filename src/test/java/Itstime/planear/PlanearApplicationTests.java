d52a6156ca3865e5042c133dcac62951050de530
hyeon-gyu <hyungue729@naver.com>
2024-03-22 20:40:08 +0900

    Init Project

---
+++ src/test/java/Itstime/planear/PlanearApplicationTests.java
@@ -0,0 +1,13 @@
+package Itstime.planear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
