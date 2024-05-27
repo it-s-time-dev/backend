1c1c60ad6cc8843c6fddebad1133e760288a630e
hyeon-gyu <hyungue729@naver.com>
2024-05-28 00:07:30 +0900

    feat: 스웨거 적용

--- src/main/java/Itstime/planear/shop/controller/ShopController.java
+++ src/main/java/Itstime/planear/shop/controller/ShopController.java
@@ -3,10 +3,13 @@ package Itstime.planear.shop.controller;
 import Itstime.planear.common.ApiResponse;
 import Itstime.planear.shop.dto.response.ItemListResponseDto;
 import Itstime.planear.shop.service.ShopService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "상점 컨트롤러", description = "상점 관련 API입니다.")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/avatar")
@@ -15,8 +18,8 @@ public class ShopController {
     private final ShopService shopService;
 
 
-    // 카테고리별 아이템 목록 조회
     @GetMapping("/store/{categoryId}")
+    @Operation(summary = "카테고리별 아이템 목록 조회", description = "FACE(1), HAIR(2), TOP(3), BOTTOM(4), SHOES(5), ACCESSORY(6)")
     public ApiResponse<ItemListResponseDto> itemListByCategory(
             @PathVariable(name = "categoryId", required = true) Long categoryId,
             @RequestHeader(name = "user-no", required = false) Long memberId
