795aabfba6e9a071bd2f8e0ecf046f7ad51fa9e3
bangyewon <byewon1111@gmail.com>
2024-06-02 12:26:32 +0900

    feat : 스웨거 설명란 작성

--- src/main/java/Itstime/planear/schedule/Controller/ScheduleController.java
+++ src/main/java/Itstime/planear/schedule/Controller/ScheduleController.java
@@ -23,26 +23,29 @@ public class ScheduleController {
     @Operation(summary = "일정 추가", description = "일정 추가 API")
     public ApiResponse<ScheduleResponseDTO.ScheduleCreateDTO> create(
             @RequestBody ScheduleRequestDTO.ScheduleCreateDTO scheduleCreateDTO,
-            @RequestHeader(value = "user-no",required = false) Long memberId) {
-        ScheduleResponseDTO.ScheduleCreateDTO result = scheduleService.create(memberId,scheduleCreateDTO);
+            @RequestHeader(value = "user-no", required = false) Long memberId) {
+        ScheduleResponseDTO.ScheduleCreateDTO result = scheduleService.create(memberId, scheduleCreateDTO);
         return ApiResponse.success(result);
     }
+
     @PutMapping("/schedule/{scheduleId}")
     @Operation(summary = "일정 수정", description = "일정 관련 title,detail,categoryId,start,end 수정 가능 API")
     public ApiResponse<ScheduleResponseDTO.ScheduleUpdateDTO> update(
             @PathVariable("scheduleId") Long scheduleId,
             @RequestBody ScheduleRequestDTO.ScheduleUpdateDTO scheduleUpdateDTO,
-            @RequestHeader(value = "user-no",required = false) Long memberId) {
-        ScheduleResponseDTO.ScheduleUpdateDTO result = scheduleService.update(memberId,scheduleId,scheduleUpdateDTO);
+            @RequestHeader(value = "user-no", required = false) Long memberId) {
+        ScheduleResponseDTO.ScheduleUpdateDTO result = scheduleService.update(memberId, scheduleId, scheduleUpdateDTO);
         return ApiResponse.success(result);
     }
+
     @PostMapping("/schedule/complete")
     @Operation(summary = "일정 완료", description = "일정 완료시 토큰 발행 API")
-    public ApiResponse<ScheduleResponseDTO.scheduleCompleteDTO> complete(@RequestBody ScheduleRequestDTO.ScheduleCompleteDTO scheduleCompleteDTO,
-            @RequestHeader(value = "user-no",required = false) Long memberId){
-                ScheduleResponseDTO.scheduleCompleteDTO result = scheduleService.complete(memberId, scheduleCompleteDTO.getScheduleId());
-                return ApiResponse.success(result);
+    public ApiResponse<ScheduleResponseDTO.ScheduleCompleteDTO> complete(@RequestBody ScheduleRequestDTO.ScheduleCompleteDTO scheduleCompleteDTO,
+                                                                         @RequestHeader(value = "user-no", required = false) Long memberId) {
+        ScheduleResponseDTO.ScheduleCompleteDTO result = scheduleService.complete(memberId, scheduleCompleteDTO.getScheduleId());
+        return ApiResponse.success(result);
     }
+
     @GetMapping("/schedule")
     @Operation(summary = "먼슬리 일정 조회", description = "startInclusive,endInclusive통해 원하는 기간 내 일정 조회 API")
     public ApiResponse<List<ScheduleResponseDTO.ScheduleFindAllDTO>> findAll(
@@ -53,6 +56,7 @@ public class ScheduleController {
         List<ScheduleResponseDTO.ScheduleFindAllDTO> result = scheduleService.findAll(memberId, startInclusive, endInclusive);
         return ApiResponse.success(result);
     }
+
     @GetMapping("/schedule/detail")
     @Operation(summary = "하루 상세 일정 조회", description = "targetDay로 선택한 날 상세 일정 조회 API")
     public ApiResponse<List<ScheduleResponseDTO.ScheduleFindOneDTO>> findOne(
@@ -62,4 +66,13 @@ public class ScheduleController {
         List<ScheduleResponseDTO.ScheduleFindOneDTO> result = scheduleService.findOne(memberId, targetDay);
         return ApiResponse.success(result);
     }
+
+    @DeleteMapping("/schedule/{scheduleId}")
+    @Operation(summary = "일정 삭제", description = "일정 삭제 API")
+    public ApiResponse<ScheduleResponseDTO.ScheduleDeleteDTO> delete(@PathVariable("scheduleId") Long scheduleId,
+                                                                     @RequestHeader(value = "user-no", required = false) Long memberId) {
+        ScheduleResponseDTO.ScheduleDeleteDTO result = scheduleService.delete(memberId, scheduleId);
+        return ApiResponse.success(result);
+    }
 }
+
