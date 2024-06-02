e006ea0c72b1b8520931975a5ef7f1fe60861a43
hyeon-gyu <hyungue729@naver.com>
2024-06-02 11:15:57 +0900

    #74 feat: 스웨거 설명란 작성

--- src/main/java/Itstime/planear/coin/controller/CoinController.java
+++ src/main/java/Itstime/planear/coin/controller/CoinController.java
@@ -3,17 +3,21 @@ package Itstime.planear.coin.controller;
 import Itstime.planear.coin.dto.CoinAmountResponse;
 import Itstime.planear.coin.service.CoinService;
 import Itstime.planear.common.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "코인 조회 컨트롤러", description = "코인 조회 API입니다.")
 @RequiredArgsConstructor
 @RestController
 public class CoinController {
     private final CoinService coinService;
 
     @GetMapping("/user/coin")
+    @Operation(summary = "멤버 코인 조회", description = "사용자별 보유 코인 조회 API")
     public ApiResponse<CoinAmountResponse> getCoinAmount(@RequestHeader(name = "user-no", required = false) Long memberId) {
         return ApiResponse.success(coinService.getCoinAmount(memberId));
     }
--- src/main/java/Itstime/planear/member/controller/MemberController.java
+++ src/main/java/Itstime/planear/member/controller/MemberController.java
@@ -6,10 +6,13 @@ import Itstime.planear.member.dto.CheckMemberNameResponse;
 import Itstime.planear.member.dto.CreateMemberRequest;
 import Itstime.planear.member.dto.CreateMemberResponse;
 import Itstime.planear.member.service.MemberService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "멤버 컨트롤러", description = "멤버 정보 관련 API입니다.")
 @RestController
 public class MemberController {
 
@@ -19,6 +22,7 @@ public class MemberController {
         this.memberService = memberService;
     }
 
+    @Operation(summary = "사용자명 중복 조회", description = "사용자 이름 중복 여부 확인 API")
     @PostMapping("/user/check")
     public ApiResponse<CheckMemberNameResponse> checkMemberName(
             @RequestBody CheckMemberNameRequest request
@@ -26,6 +30,7 @@ public class MemberController {
         return memberService.checkMemberName(request);
     }
 
+    @Operation(summary = "사용자명 저장", description = "사용자 생성 및 사용자명 저장 API")
     @PostMapping("/user")
     public ApiResponse<CreateMemberResponse> createMember(
             @RequestBody CreateMemberRequest request
