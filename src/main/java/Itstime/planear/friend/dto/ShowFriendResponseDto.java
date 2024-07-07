67524ba9c7a52fec87ea584998f5592fac4dc6c8
bangyewon <90364660+bangyewon@users.noreply.github.com>
2024-07-07 18:06:54 +0900

    refactor : response 값 수정

--- src/main/java/Itstime/planear/friend/dto/ShowFriendResponseDto.java
+++ src/main/java/Itstime/planear/friend/dto/ShowFriendResponseDto.java
@@ -9,11 +9,11 @@ import java.util.stream.Collectors;
 
 @Getter
 public class ShowFriendResponseDto {
-    String name;
+    String nickname;
     private List<MyItemProcessDto> wearingLists;
 
     public ShowFriendResponseDto(Member member) {
-        this.name = member.getMemberName().getName();
+        this.nickname = member.getMemberName().getName();
         this.wearingLists = member.getWearingList().stream()
                 .map(wearing -> new MyItemProcessDto(wearing.getId(), wearing.getItem().getImg_url(), wearing.getBodyPart()))
                 .collect(Collectors.toList());
