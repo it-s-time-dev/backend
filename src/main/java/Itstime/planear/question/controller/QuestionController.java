0228985009cb40d073b78e18079694a4e66da180
hyeon-gyu <hyungue729@naver.com>
2024-07-14 19:50:14 +0900

    #142 feat: 누락된 스웨거 Tag 반영 작업

--- src/main/java/Itstime/planear/question/controller/QuestionController.java
+++ src/main/java/Itstime/planear/question/controller/QuestionController.java
@@ -5,6 +5,7 @@ import Itstime.planear.question.dto.CreateQuestionRequest;
 import Itstime.planear.question.dto.CreateQuestionResponse;
 import Itstime.planear.question.dto.GetQuestionsResponse;
 import Itstime.planear.question.service.QuestionService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+@Tag(name = "소셜 질문 컨트롤러", description = "소셜 질문 관련 API입니다.")
 @RequiredArgsConstructor
 @RestController
 public class QuestionController {
